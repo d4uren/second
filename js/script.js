9aefcebf8c40c541d855a873893ee0db5fda57ef
Dauren <dauren15r@gmail.com>
2021-01-25 23:23:28 +0600

    correct_commit_js

--- js/script.js
+++ js/script.js
@@ -18,8 +18,18 @@ personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
-if (4 == 4) {
+if (4 == 5) {
     console.log('Ok');
 } else {
     console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
 }
