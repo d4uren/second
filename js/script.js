b35c12b80e45fdb559564a64438819451f31e1d8
Dauren <dauren15r@gmail.com>
2021-03-02 01:09:57 +0600

    homework.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const NumberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -60,4 +62,21 @@ let num = 50;
 while (num < 55) {
     console.log(num);
     num++
+}
+
+
+let num = 50;
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55)
+
+
+let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
 }
